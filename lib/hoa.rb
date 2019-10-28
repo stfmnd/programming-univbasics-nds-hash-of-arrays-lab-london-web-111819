6dc66c598a676737b933e1febd7999c81d988afb
Matthew <matthew.97.clark@gmail.com>
2019-10-28 20:00:20 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,8 +6,7 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  BASE_HOA[:chipmunks].push("Dave")
-  BASE_HOA[:chipmunks]
-  
-  BASE_HOA[:jetsons].push("")
+  BASE_HOA[show].push(name)
+  BASE_HOA[show]
+
 end
